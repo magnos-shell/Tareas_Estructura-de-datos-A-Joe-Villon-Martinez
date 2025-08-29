7c8002a732c3dfde196772ee7598b63fe5380e88
Joe Villon Martinez <jm.villonm@uea.edu.ec>
2025-08-29 11:37:34 -0500

    codigo Parte 1

---
+++ traductor_semana 11/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    // Diccionarios para traducir en ambas direcciones
+    static System.Collections.Generic.Dictionary<string, string> englishToSpanish = new System.Collections.Generic.Dictionary<string, string>();
+    static System.Collections.Generic.Dictionary<string, string> spanishToEnglish = new System.Collections.Generic.Dictionary<string, string>();
+
+    static void Main(string[] args)
+    {
+        InicializarDiccionario();
+        MostrarMenu();
+    }
+
+    static void InicializarDiccionario()
+    {
+        // Palabras base del diccionario
+        string[,] palabras = {
+            {"time", "tiempo"},
+            {"person", "persona"},
+            {"year", "año"},
+            {"way", "camino"},
+            {"day", "día"},
+            {"thing", "cosa"},
+            {"man", "hombre"},
+            {"world", "mundo"},
+            {"life", "vida"},
+            {"hand", "mano"},
+            {"part", "parte"},
+            {"child", "niño"},
+            {"eye", "ojo"},
+            {"woman", "mujer"},
+            {"place", "lugar"},
+            {"work", "trabajo"},
+            {"week", "semana"},
+            {"case", "caso"},
+            {"point", "punto"},
+            {"government", "gobierno"},
+            {"company", "empresa"}
+        };
